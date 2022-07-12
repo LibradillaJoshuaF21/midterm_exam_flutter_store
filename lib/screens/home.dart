63edce57ad09528d02a5ceab8dde519777ee5c46
Joshua Libradilla <joshlibra52tm@gmail.com>
2022-07-12 12:23:05 +0800

    Implemented getAllProducts
    Implemented getAllProducts in services and home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 import '../models/product.dart';
 import '../services/api_service.dart';
@@ -8,6 +9,13 @@ import 'product_detail.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+  ApiService get service => GetIt.I<ApiService>();
+
+  Future<List<Product>> getAllProducts() async {
+    final result = await service.getAllProducts();
+    return result;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,13 +57,13 @@ class HomeScreen extends StatelessWidget {
                 itemBuilder: ((context, index) {
                   final product = snapshot.data![index];
                   return ListTile(
-                    title: Text('[title]'),
+                    title: Text(product.title!),
                     leading: Image.network(
                       '[image]',
                       height: 50,
                       width: 50,
                     ),
-                    subtitle: Text('\$price}'),
+                    subtitle: Text('\$${product.price}'),
                     onTap: () {
                       Navigator.push(
                         context,
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: avoid_print, invalid_return_type_for_catch_error
+
+import 'package:fake_store/models/product.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
@@ -5,12 +8,25 @@ class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
 
   Future<dynamic> login(String userName, String password) {
-    return http.post(Uri.parse("${baseUrl}auth/login"),
+    return http.post(Uri.parse("$baseUrl/auth/login"),
         body: {'username': userName, 'password': password}).then((data) {
       if (data.statusCode == 201) {
         final jsonData = json.decode(data.body);
         return Future<dynamic>(jsonData);
       }
-    });
+    }).catchError((err) => print(err));
+  }
+
+  Future<List<Product>> getAllProducts() async {
+    return http.get(Uri.parse('$baseUrl/products')).then((data) {
+      final productList = <Product>[];
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        for (var item in jsonData) {
+          productList.add(Product.fromJson(item));
+        }
+      }
+      return productList;
+    }).catchError((err) => print(err));
   }
 }
